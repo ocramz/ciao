ff12e9517abaa401af10a43af12cfbbec95c84cf
ocramz <zocca.marco@gmail.com>
2021-01-30 23:07:47 +0100

    now it works

--- src/Server.hs
+++ src/Server.hs
@@ -1,10 +1,16 @@
+{-# LANGUAGE OverloadedStrings #-}
 {-# options_ghc -Wno-unused-imports #-}
 module Server where
 
+import Control.Monad.IO.Class (MonadIO(..))
 import Data.Function ((&))
-import Network.Wai.Middleware.Static (staticPolicy, only)
+import Data.List (intersperse)
+import Network.Wai.Middleware.Static (staticPolicy, only, addBase)
 import Network.Wai.Handler.Warp (setPort, defaultSettings)
-import Web.Scotty.Trans (ScottyT, scottyOptsT, Options(..), middleware, get)
+import Network.Wai.Middleware.RequestLogger (logStdout)
+import System.Directory (listDirectory)
+import Data.Text.Lazy (Text, pack, unpack)
+import Web.Scotty.Trans (ScottyT, scottyOptsT, Options(..), middleware, get, text)
 
 import CLI (cli, CLIOpts(..))
 
@@ -15,7 +21,16 @@ server = do
   let
     setts = defaultSettings &
             setPort p
-  scottyOptsT (Options 0 setts) id $ route dir
+  scottyOptsT (Options 0 setts) id $ do
+    middleware logStdout
+    lsDir dir
+    route dir
 
 route :: String -> ScottyT e m ()
-route dir = middleware (staticPolicy $ only [("/", dir)])
+route dir =
+  middleware $ staticPolicy (addBase dir)
+
+lsDir :: (MonadIO m) => FilePath -> ScottyT Text m ()
+lsDir dir = get "/" $ do
+  ls <- liftIO $ listDirectory dir
+  text (pack $ unwords $ intersperse "\n" ls)
